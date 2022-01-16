24e035cb99348e262a9f05a5db7d3925b4ecf754
Som's Laptop <somaiahuthappa@gmail.com>
2022-01-16 00:31:55 -0500

    initial commit

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,18 @@
-window.alert("This is an alert! JavaScript is running!")
+// window.alert("This is an alert! JavaScript is running!")
+
+// This creates a function named "fight"
+
+var playerName = window.prompt("What is your robot's name?");
+
+console.log(playerName);
+console.log("This logs a string, good for leaving yourself a message");
+// this does math
+console.log(10 + 10);
+// what is this?
+console.log("Our robot name is " + playerName)
+
+function fight () {
+    window.alert("The fight has bugun!")
+}
+
+// fight ();
