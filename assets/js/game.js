e57e33cba4b1b64a80a510ec3ec87d8523fac821
Som's Laptop <somaiahuthappa@gmail.com>
2022-01-19 00:56:49 -0500

    added highscore

--- assets/js/game.js
+++ assets/js/game.js
@@ -149,6 +149,24 @@ var endGame = function() {
     window.alert("You've lost your robot in battle!");
   }
 
+   // check localStorage for high score, if it's not there, use 0
+   var highScore = localStorage.getItem("highscore");
+   if (highScore === null) {
+     highScore = 0;
+   }
+ 
+   // if player has more money than the highScore, player has new high score
+ 
+   if (playerInfo.money > highScore) {
+     localStorage.setItem("highscore", playerInfo.money);
+     localStorage.setItem("name", playerInfo.name);
+     alert(playerInfo.name + " now has a high score of " + playerInfo.money + "!");
+   }
+   else {
+     alert(playerInfo.name + " did not beat the highscore of " + highScore + ". Maybe next time!");
+   }
+ 
+
   // ask player if they'd like to play again
   var playAgainConfirm = window.confirm('Would you like to play again?');
 
